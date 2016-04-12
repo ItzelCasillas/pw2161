e70104420e56b967a3eefbbc1b52d588a4ee8448
ItzelCasillas <1tz3lcg@gmail.com>
2016-04-12 10:59:27 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,13 @@
+var botonClic = function()
+{
+	alert("Clic del botón")
+}
+// function botonClic()
+// {
+// 	alert("Clic del botón");
+// }
+var nombre = "Itzel";
+document.write("<strong>JS en acción</strong>"
+	    	           + " "+nombre);
+console.log("A punto de lanzar un mensaje");
+alert("Mensaje por pantalla");
